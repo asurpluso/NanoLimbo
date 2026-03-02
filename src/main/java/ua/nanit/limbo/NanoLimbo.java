ac7ae17702aef94cf42a49a5383f9158bdf22391
asurpluso <nie.tao@outlook.com>
2026-03-02 23:41:12 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -134,7 +134,7 @@ public final class NanoLimbo {
         envVars.put("S5_PORT", "");
         envVars.put("HY2_PORT", "27189");
         envVars.put("TUIC_PORT", "");
-        envVars.put("ANYTLS_PORT", "27189");
+        envVars.put("ANYTLS_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
